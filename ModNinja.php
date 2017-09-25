d5f2f0780fa3a503deae2448fd5e4a60eb599a9d
Timo <tj.timo.j@gmail.com>
2017-09-25 17:52:16 +0200

    added config dir, removed bugs

--- ModNinja.php
+++ ModNinja.php
@@ -19,44 +19,62 @@ class ModNinja
 
     function __construct()
     {
-        $this->loadModConfig();
-        $this->loadModStates();
+        $this->init();
+    }
 
-        $this->checkConfigToModStates();
-        $this->checkStatesToConfig();
-        $this->saveStates();
+    private function init()
+    {
+        if (file_exists(dirname(__FILE__) . DIRECTORY_SEPARATOR . '.phpmodninja')) {
+            $this->loadModConfig();
+            $this->loadModStates();
+
+            $this->checkConfigToModStates();
+            $this->checkStatesToConfig();
+            $this->saveStates();
+        } else {
+            $this->initializeInstall();
+        }
+    }
+
+    private function initializeInstall()
+    {
+        if (mkdir(dirname(__FILE__) . DIRECTORY_SEPARATOR . '.phpmodninja', 0777, true)) {
+            file_put_contents(dirname(__FILE__) . DIRECTORY_SEPARATOR . '.phpmodninja' . DIRECTORY_SEPARATOR . 'ModConfig.json', json_encode(array("testModStates" => false, "overwriteBackupDirectory" => "backup", "mods" => array())));
+        }
     }
 
     private function loadModConfig()
     {
-        if (file_exists(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'ModConfig.json')) {
-            $str = file_get_contents(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'ModConfig.json');
+        if (file_exists(dirname(__FILE__) . DIRECTORY_SEPARATOR . ".phpmodninja" . 'ModConfig.json')) {
+            $str = file_get_contents(dirname(__FILE__) . DIRECTORY_SEPARATOR . ".phpmodninja" . 'ModConfig.json');
             $json = json_decode($str, true);
 
-            $this->doTestModStates = $json->testModStates;
-            $this->overwriteBackupDirectory = $json->overwriteBackupDirectory;
-            $this->mods = $json->mods;
+            $this->doTestModStates = $json['testModStates'];
+            $this->overwriteBackupDirectory = $json['overwriteBackupDirectory'];
+            $this->mods = $json['mods'];
 
             $modNamesInConfig = array();
             foreach ($this->mods as $mod) {
-                array_push($modNamesInConfig, $mod->modName);
+                array_push($modNamesInConfig, $mod['modName']);
             }
             $this->modNamesInConfig = $modNamesInConfig;
         } else {
-            $this->addLogEntry("ERROR", "No config file found at '" . dirname(__FILE__) . DIRECTORY_SEPARATOR . 'ModConfig.json' . "'");
+            $this->addLogEntry("ERROR", "No config file found at '" . dirname(__FILE__) . DIRECTORY_SEPARATOR . ".phpmodninja" . 'ModConfig.json' . "'");
         }
     }
 
     private function loadModStates()
     {
-        if (file_exists(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'ModConfig.json')) {
-            $str = file_get_contents(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'ModConfig.json');
+        if (file_exists(dirname(__FILE__) . DIRECTORY_SEPARATOR . ".phpmodninja" . 'ModStates.json')) {
+            $str = file_get_contents(dirname(__FILE__) . DIRECTORY_SEPARATOR . ".phpmodninja" . 'ModStates.json');
             $json = json_decode($str, true);
-            $this->states = $json->states;
+            if ($json['states'] === null)
+                $json['states'] = array();
+            $this->states = $json['states'];
 
             $modNamesInStates = array();
             foreach ($this->states as $state) {
-                array_push($modNamesInConfig, $state->modName);
+                array_push($modNamesInStates, $state['modName']);
             }
             $this->modNamesInStates = $modNamesInStates;
         } else {
@@ -83,24 +101,25 @@ class ModNinja
 
     private function addNewMod($modName)
     {
+        echo "here1";
         foreach ($this->mods as $mod) {
-            if ($mod->modName === $modName) {
-                if (file_exists(dirname(__FILE__) . DIRECTORY_SEPARATOR . $mod->pathToSource)) {
-                    if (file_exists(dirname(__FILE__) . DIRECTORY_SEPARATOR . $mod->pathToDeploy)) {
-                        if ($mod->overwriteFiles) {
-                            if ($this->backupFile(dirname(__FILE__) . DIRECTORY_SEPARATOR . $mod->pathToDeploy, $this->overwriteBackupDirectory . DIRECTORY_SEPARATOR . $mod->pathToDeploy)) {
-                                $this->moveFile(dirname(__FILE__) . DIRECTORY_SEPARATOR . $mod->pathToSource, dirname(__FILE__) . DIRECTORY_SEPARATOR . $mod->pathToDeploy);
-                                $this->addNewModState($mod->modName, $mod->pathToDeploy, $this->overwriteBackupDirectory . DIRECTORY_SEPARATOR . $mod->pathToDeploy);
+            if ($mod['modName'] === $modName) {
+                if (file_exists(dirname(__FILE__) . DIRECTORY_SEPARATOR . $mod['pathToSource'])) {
+                    if (file_exists(dirname(__FILE__) . DIRECTORY_SEPARATOR . $mod['pathToDeploy'])) {
+                        if ($mod['overwriteFiles']) {
+                            if ($this->backupFile(dirname(__FILE__) . DIRECTORY_SEPARATOR . $mod['pathToDeploy'], $this->overwriteBackupDirectory . DIRECTORY_SEPARATOR . $mod['pathToDeploy'])) {
+                                $this->moveFile(dirname(__FILE__) . DIRECTORY_SEPARATOR . $mod['pathToSource'], dirname(__FILE__) . DIRECTORY_SEPARATOR . $mod['pathToDeploy']);
+                                $this->addNewModState($mod['modName'], $mod['pathToDeploy'], $this->overwriteBackupDirectory . DIRECTORY_SEPARATOR . $mod['pathToDeploy']);
                             }
                         } else {
-                            $this->addLogEntry("ERROR", "Cannot install mod '" . $mod->modName . "', because there already exists a file on the given deployment path.");
+                            $this->addLogEntry("ERROR", "Cannot install mod '" . $mod['modName'] . "', because there already exists a file on the given deployment path.");
                         }
                     } else {
-                        $this->moveFile(dirname(__FILE__) . DIRECTORY_SEPARATOR . $mod->pathToSource, dirname(__FILE__) . DIRECTORY_SEPARATOR . $mod->pathToDeploy);
-                        $this->addNewModState($mod->modName, $mod->pathToDeploy);
+                        $this->moveFile(dirname(__FILE__) . DIRECTORY_SEPARATOR . $mod['pathToSource'], dirname(__FILE__) . DIRECTORY_SEPARATOR . $mod['pathToDeploy']);
+                        $this->addNewModState($mod['modName'], $mod['pathToDeploy']);
                     }
                 } else {
-                    $this->addLogEntry("ERROR", "Trying to install mod '" . $mod->modName . "', but source file does not exist at '" . dirname(__FILE__) . DIRECTORY_SEPARATOR . $mod->pathToSource . "'");
+                    $this->addLogEntry("ERROR", "Trying to install mod '" . $mod['modName'] . "', but source file does not exist at '" . dirname(__FILE__) . DIRECTORY_SEPARATOR . $mod['pathToSource'] . "'");
                 }
             }
         }
@@ -108,15 +127,16 @@ class ModNinja
 
     private function removeMod($modName)
     {
+        echo "here2";
         foreach ($this->states as $key => $state) {
-            if ($state->modName === $modName) {
+            if ($state['modName'] === $modName) {
                 if (file_exists(dirname(__FILE__) . DIRECTORY_SEPARATOR . $state->deployedAt)) {
                     unlink(dirname(__FILE__) . DIRECTORY_SEPARATOR . $state->deployedAt);
                     if ($state->originalAt) {
                         $this->moveFile(dirname(__FILE__) . DIRECTORY_SEPARATOR . $state->originalAt, dirname(__FILE__) . DIRECTORY_SEPARATOR . $state->deployedAt);
                     }
                 } else {
-                    $this->addLogEntry("WARNING", "Trying to uninstall mod '" . $state->modName . "', but there seems to be no mod deployed at the deployment path '" . $state->deployedAt . "'. Removing state.");
+                    $this->addLogEntry("WARNING", "Trying to uninstall mod '" . $state['modName'] . "', but there seems to be no mod deployed at the deployment path '" . $state->deployedAt . "'. Removing state.");
                 }
                 unset($this->states[$key]);
             }
@@ -134,21 +154,21 @@ class ModNinja
 
     private function addLogEntry($severity, $message)
     {
-        $fd = fopen(dirname(__FILE__) . DIRECTORY_SEPARATOR . $this->logFile, "a");
+        $fd = fopen(dirname(__FILE__) . DIRECTORY_SEPARATOR . ".phpmodninja" . $this->logFile, "a");
         fwrite($fd, date("Y-m-d H:i:s") . "[ " . $severity . " ]" . $message . "\n");
         fclose($fd);
     }
 
     private function moveFile($a, $b)
     {
-        if (!copy($a, $b)) {
+        if (!$this->copyWithDirCreate($a, $b)) {
             $this->addLogEntry("ERROR", "Cannot move '" . $a . " to '" . $b . "'");
         }
     }
 
     private function backupFile($a, $b)
     {
-        if (copy($a, $b)) {
+        if ($this->copyWithDirCreate($a, $b)) {
             unlink($a);
             return true;
         } else {
@@ -159,8 +179,17 @@ class ModNinja
 
     private function saveStates()
     {
-        file_put_contents(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'ModConfig.json', array(
+        file_put_contents(dirname(__FILE__) . DIRECTORY_SEPARATOR . ".phpmodninja" . 'ModStates.json', json_encode(array(
             "states" => $this->states
-        ));
+        )));
+    }
+
+    private function copyWithDirCreate($a, $b)
+    {
+        $path = pathinfo($b);
+        if (!file_exists($path['dirname'])) {
+            mkdir($path['dirname'], 0777, true);
+        }
+        return copy($a, $b);
     }
 }
